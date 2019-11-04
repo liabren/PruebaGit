d9de76dafca0981bdaadcc4e99ce5c43b6dad428
Lía Brenkier <liabrenkier@gmail.com>
2019-11-04 01:06:04 -0300

    Funcion saludo mejorada

---
+++ .history/intento_20191104010502.js
@@ -0,0 +1,10 @@
+
+function saludar(dia) {
+    if (dia != "Sabado" && dia != "Domingo") {
+        console.log("Hola, buen día!!");
+    }else{
+        console.log("Buen fin de semana");
+    }
+}
+
+saludar("Domingo")
